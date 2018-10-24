3dac525dcb37c20a266e887caae5513c62d7f316
Chris Dopuch <chris.dopuch@gmail.com>
2018-10-23 22:11:00 -0700

    image dir

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
-import me from './me.jpg';
+import me from './images/me.jpg';
 
 const stylesDeclarations = (theme: Theme) => createStyles({
   '@keyframes face-spin': {
