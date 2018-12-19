5df465610b35cd0b1e09a4b0fe9e1cfd8a6c1623
Chris Dopuch <chris.dopuch@gmail.com>
2018-12-19 00:11:56 -0800

    added several more photos

--- src/components/pages/Gallery.tsx
+++ src/components/pages/Gallery.tsx
@@ -5,11 +5,53 @@ import Typography from '@material-ui/core/Typography';
 import React, { SFC } from 'react';
 import GridGallery from 'react-grid-gallery';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
-import beast09 from '../../images/beast-09-front.jpg';
-import beast09Thumb from '../../thumbnails/beast-09-front_thumb.jpg';
+import alexiaValachev from '../../images/alexia-valachev.jpg';
+import beast09 from '../../images/beast-09.jpg';
+import butcher from '../../images/butcher.jpg';
+import juggernaut from '../../images/juggernaut.jpg';
+import mandy from '../../images/mandy.jpg';
+import ruin from '../../images/ruin.jpg';
+import vlad from '../../images/vlad.png';
+import widowmakers from '../../images/widowmakers.jpg';
+import alexiaValachevThumb from '../../thumbnails/alexia-valachev_thumb.jpg';
+import beast09Thumb from '../../thumbnails/beast-09_thumb.jpg';
+import butcherThumb from '../../thumbnails/butcher_thumb.jpg';
+import juggernautThumb from '../../thumbnails/juggernaut_thumb.jpg';
+import mandyThumb from '../../thumbnails/mandy_thumb.jpg';
+import ruinThumb from '../../thumbnails/ruin_thumb.jpg';
+import vladThumb from '../../thumbnails/vlad_thumb.png';
+import widowmakersThumb from '../../thumbnails/widowmakers_thumb.jpg';
 import usePageView from '../hooks/usePageView';
 
-const IMAGES = [
+function shuffleArray(array: any[]) {
+  let currentIndex = array.length;
+  let temporaryValue = currentIndex;
+  let randomIndex = currentIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+const IMAGES = shuffleArray([
+  {
+    caption: 'Juggernaut (Warmachine)',
+    src: juggernaut,
+    thumbnail: juggernautThumb,
+    thumbnailHeight: 169,
+    thumbnailWidth: 300,
+  },
+  {
+    caption: 'Vladimir (a Pathfinder OC of mine)',
+    src: vlad,
+    thumbnail: vladThumb,
+    thumbnailHeight: 404,
+    thumbnailWidth: 300,
+  },
   {
     caption: 'Beast 09 (Warmachine)',
     src: beast09,
@@ -17,7 +59,42 @@ const IMAGES = [
     thumbnailHeight: 169,
     thumbnailWidth: 300,
   },
-];
+  {
+    caption: 'Widowmakers (Warmachine)',
+    src: widowmakers,
+    thumbnail: widowmakersThumb,
+    thumbnailHeight: 169,
+    thumbnailWidth: 300,
+  },
+  {
+    caption: 'The Butcher (Warmachine)',
+    src: butcher,
+    thumbnail: butcherThumb,
+    thumbnailHeight: 169,
+    thumbnailWidth: 300,
+  },
+  {
+    caption: 'Mandy (from the movie Mandy)',
+    src: mandy,
+    thumbnail: mandyThumb,
+    thumbnailHeight: 200,
+    thumbnailWidth: 300,
+  },
+  {
+    caption: 'Ruin (Warmachine)',
+    src: ruin,
+    thumbnail: ruinThumb,
+    thumbnailHeight: 169,
+    thumbnailWidth: 300,
+  },
+  {
+    caption: 'Alexia and Valachev (Warmachine)',
+    src: alexiaValachev,
+    thumbnail: alexiaValachevThumb,
+    thumbnailHeight: 225,
+    thumbnailWidth: 300,
+  },
+]);
 
 const stylesDeclarations = (theme: Theme) =>
   createStyles({
